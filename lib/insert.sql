9a02e9d552581b3388925983a9b10a49a85f2d1e
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-22 00:13:14 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 10, 'M', 'brown', 'happy', 1);
+
+insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 5, 'F', 'black', 'nice', 1);
+
+insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 20, 'F', 'white', 'sleepy', 0);
+
+insert into bears (name, age, gender, color, temperament, alive) values ("Sergeant Brown", 17, 'M', 'brown', 'remorseful', 1);
+
+insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 11, 'M', 'black', 'wrathful', 0);
+
+insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 17, 'F', 'brown', 'prideful', 1);
+
+insert into bears (name, age, gender, color, temperament, alive) values ("Wendy", 20, 'M', 'brown', 'greedy', 0);
+
+insert into bears (name, age, gender, color, temperament, alive) values (null, 10, 'F', 'white', 'hungry', 1);
